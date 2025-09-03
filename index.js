87ff552a54898e9c6e4a975b89d3bbe97c7a3738
vivek kumar <vivekr4400@gmail.com>
2025-09-03 17:44:08 +0530

    increase the upload limit for express server

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.use((req, res, next) => {
     }
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use('/api/posts', require('./src/routes/posts.routes'));
 app.use('/api/upload', require('./src/routes/upload.routes'));
